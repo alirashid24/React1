3c4fd4cf6cf9718b5caccf458305b3a10f2d4dbd
Rashid ALI <ali.7860rashid@gmail.com>
2024-01-15 09:28:00 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,24 @@
+// // const heading = React.createElement(
+// //     "h1",
+// //     { id: "heading" }, "Hello world from React");
+
+// // console.log(heading);
+
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")]),
+
+    React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")])])
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
+
