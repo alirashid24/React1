d221011ca29fea6dc52cc2f093d1e53891e11112
Rashid ALI <ali.7860rashid@gmail.com>
2024-01-18 02:26:46 +0530

    episdoe-02

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-// // const heading = React.createElement(
-// //     "h1",
-// //     { id: "heading" }, "Hello world from React");
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading" }, "Hello world from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 
-// // console.log(heading);
-
-// // const root = ReactDOM.createRoot(document.getElementById("root"));
-// // root.render(heading);
-
+import React from "react";
+import ReactDOM from "react-dom";
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child1" },
         [React.createElement("h1", {}, "I am h1 tag"),
@@ -19,6 +18,3 @@ const parent = React.createElement("div", { id: "parent" }, [
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-
-
-
