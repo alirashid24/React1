c82a3025fa130feb547ef1411988a9f900f43229
Rashid Ali <ali.7860rashid@gmail.com>
2024-01-29 10:51:11 +0530

    episode -03

--- App.js
+++ App.js
@@ -6,15 +6,20 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")]),
+// const parent = React.createElement("div", { id: "parent" }, [
+//     React.createElement("div", { id: "child1" },
+//         [React.createElement("h1", {}, "I am h1 tag"),
+//         React.createElement("h2", {}, "I am h2 tag")]),
 
-    React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag")])])
+//     React.createElement("div", { id: "child1" },
+//         [React.createElement("h1", {}, "I am h1 tag"),
+//         React.createElement("h2", {}, "I am h2 tag")])])
 
-console.log(parent);
+// console.log(parent);
+
+const jsxheading = (
+<h1>
+    Hello world by jsx
+</h1>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxheading);
